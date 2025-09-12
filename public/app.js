f6c8a34218c36836f31f1739e47f6ea77d0f73e2
VictorJAguilera <89158788+VictorJAguilera@users.noreply.github.com>
2025-09-12 18:00:23 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 // Loader limpio (producción)
 (function () {
   function loadCore() {
-    var url = '/app.main.js?v=ui2025-rt2'; // sube este número cuando cambies el core
+    var url = '/app.main.js?v=ui2025-rt3'; // <-- sube este número al desplegar
     fetch(url, { cache: 'no-store' })
       .then(function (res) { if (!res.ok) throw new Error('HTTP ' + res.status); return res.text(); })
       .then(function (code) {
